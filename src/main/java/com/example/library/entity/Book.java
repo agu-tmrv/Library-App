9a36f606fbfd6d12d355473a8c9dbe1ad181b3bf
aghalar <agalar.teymurov@gmail.com>
2025-03-21 17:20:33 +0400

    creating entity file.

--- src/main/java/com/example/library/entity/Book.java
+++ src/main/java/com/example/library/entity/Book.java
@@ -1,9 +1,76 @@
 package com.example.library.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "book")
 public class Book {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "cost")
+    private String cost;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "genre")
+    private String genre;
+
+    public Book(){
+
+    }
+
+    public Book(String title, String cost, String author, String genre) {
+        this.title = title;
+        this.cost = cost;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
